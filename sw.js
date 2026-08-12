fb01c734f78b5d89e1b571267e11e3568b8154b6
BIg-Hero777 <wf525289834@gmail.com>
2026-08-12 11:11:05 +0800

    Upgrade Tiny Quest offline cache

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-const CACHE='tiny-quest-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','styles.css','app.js']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='tiny-quest-v2';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./favicon.svg'];
+
+self.addEventListener('install',e=>{
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener('activate',e=>{
+ e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
